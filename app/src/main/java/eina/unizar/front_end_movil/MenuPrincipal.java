48174d664bf2e404a0a6693a586f35a06926ad61
asolanas0000 <andrea.solanasvicente@gmail.com>
2021-03-04 19:22:20 +0100

    avanzada pantalla menu principal falta posicionar bien los objetos

--- app/src/main/java/eina/unizar/front_end_movil/MenuPrincipal.java
+++ app/src/main/java/eina/unizar/front_end_movil/MenuPrincipal.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -15,6 +16,9 @@ public class MenuPrincipal extends AppCompatActivity {
     private static final int OPTION_ACCEDER = 0;
     private static final int OPTION_PASSWORD = 1;
     private static final int OPTION_REGISTRO = 2;
+
+    private EditText usuario;
+    private EditText password;
 
     /**
      * Called when the activity is first created.
@@ -27,6 +31,11 @@ public class MenuPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal);
 
+        //EDIT TEXT DE USUARIO
+        usuario = findViewById(R.id.nombre_usuario);
+        //EDIT TEXT DE CONTRASEÑA
+        password = findViewById(R.id.texto_contasenya);
+
         // Botón de acceder/iniciar sesión
         Button accederButton = (Button) findViewById(R.id.acceder);
         /*accederButton.setOnClickListener(new View.OnClickListener() {
@@ -37,22 +46,22 @@ public class MenuPrincipal extends AppCompatActivity {
             }
         });*/
 
-        // Botón de mostrar listas de compra
-        Button categoriasButton = (Button) findViewById(R.id.categorias);
-        /*categoriasButton.setOnClickListener(new View.OnClickListener() {
+        // Botón de olvidar la contraseña
+        Button passwordButton = (Button) findViewById(R.id.password);
+        /*passwordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), CategoriaPad.class);
-                startActivityForResult(intent, OPTION_CATEGORIAS);
+                Intent intent = new Intent (v.getContext(), CambiarPassword.class);
+                startActivityForResult(intent, OPTION_PASSWORD);
             }
         });*/
 
-        // Botón de mostrar listas de compra
-        Button categoriasButton = (Button) findViewById(R.id.categorias);
-        /*categoriasButton.setOnClickListener(new View.OnClickListener() {
+        // Botón de registrarse
+        Button registroButton = (Button) findViewById(R.id.registro);
+        /*registroButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), CategoriaPad.class);
+                Intent intent = new Intent (v.getContext(), RegistroUsuario.class);
                 startActivityForResult(intent, OPTION_CATEGORIAS);
             }
         });*/
