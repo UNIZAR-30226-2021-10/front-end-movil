f71dbedb89c6d7b86c9570403ef48b99f1108af1
asolanas0000 <andrea.solanasvicente@gmail.com>
2021-03-06 12:08:19 +0100

    xml de pantalla juego individual hecha falta codigo java

--- app/src/main/java/eina/unizar/front_end_movil/JuegoIndividual.java
+++ app/src/main/java/eina/unizar/front_end_movil/JuegoIndividual.java
@@ -10,7 +10,7 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class JuegoIndividual extends AppCompatActivity implements OnItemSelectedListener {
+public class JuegoIndividual extends AppCompatActivity{
 
     private static final int OPTION_COMENZAR = 0;
     private static final int OPTION_ATRAS = 1;
@@ -24,44 +24,12 @@ public class JuegoIndividual extends AppCompatActivity implements OnItemSelected
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.decision_individual);
+        setContentView(R.layout.juego_individual);
         getSupportActionBar().hide();
 
-        Spinner spinner = (Spinner) findViewById(R.id.caja_opciones);
-        spinner.setOnItemSelectedListener(this);
 
 
-        // Botón de empezar partida individual
-        Button comenzarButton = (Button) findViewById(R.id.comenzar);
-        comenzarButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent (v.getContext(), JuegoIndividual.class);
-                startActivityForResult(intent, OPTION_COMENZAR);
-            }
-        });
 
-        // Botón de atrás
-        Button atrasButton = (Button) findViewById(R.id.atras);
-        atrasButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), DecisionJuego.class);
-                startActivityForResult(intent, OPTION_ATRAS);
-            }
-        });
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        /* para cuando clica una opción*/
-        // TODO: para la BD aquí se cogerá el valor del nº de rondas
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        /* */
     }
 }
 
