181bc2bf613c193cc4010985f222986bd8dad7d0
Feer93 <fernando.serrano444@gmail.com>
2021-04-17 19:32:24 +0200

    fusion de dos bitmaps funcionando

--- app/src/main/java/eina/unizar/front_end_movil/CambiarAvatar.java
+++ app/src/main/java/eina/unizar/front_end_movil/CambiarAvatar.java
@@ -3,9 +3,14 @@ package eina.unizar.front_end_movil;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -79,12 +84,42 @@ public class CambiarAvatar extends AppCompatActivity {
         super.onWindowFocusChanged(hasFocus);
         if(hasFocus){
             System.err.println(imagenAvatar.getWidth()  +  "," + imagenAvatar.getHeight());
-            //Bitmap bitmap = Bitmap.createBitmap(imagenAvatar.getWidth(),imagenAvatar.getHeight()/4,Bitmap.Config.RGB_565);
-            Bitmap bitmap = BitmapFactory.decodeResource(getBaseContext().getResources(),R.mipmap.disfraz_traje);
 
-            imagenAvatar.setImageBitmap(bitmap);
+            Bitmap bitmap = BitmapFactory.decodeResource(getBaseContext().getResources(),R.mipmap.disfraz_traje);
+            //Obtenemos el bitmap del imageview
+            BitmapDrawable bitmapOriginal = (BitmapDrawable) imagenAvatar.getDrawable();
+            Bitmap bitmapAvatarStandar = bitmapOriginal.getBitmap();
+            //Height 93 Weight 93 para el traje
+            System.err.println(bitmap.getHeight() + "," + bitmap.getWidth());
+            Bitmap bmOverlay = Bitmap.createBitmap(imagenAvatar.getWidth(),imagenAvatar.getHeight(),Bitmap.Config.RGB_565);
+            Bitmap definitivo = finalcombieimage(bitmapAvatarStandar,bitmap);
+            imagenAvatar.setImageBitmap(definitivo);
         }
+    }
+
+    public Bitmap finalcombieimage(Bitmap c, Bitmap s) {
+
+        DisplayMetrics metrics = getBaseContext().getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        Bitmap bmOverlay = Bitmap.createBitmap(imagenAvatar.getWidth(),imagenAvatar.getHeight(),Bitmap.Config.RGB_565);
+        Canvas comboImage = new Canvas(bmOverlay);
+        /*
+         TOP: indica desde empieza desde arriba 0 lo cogerá desde el margen incial de la ImageView
+               si se aumenta el margen top la recortara de arriba hacia abajo (la estrecha)
+          BOTTOM: le indica hasta donde tiene que coger el alto de la Imageview
+          RIGHT: indica donde acaba o hasta donde va el margen derecho, tendrá que el total del ancho
+                 del imageview para que abarque la imageview entera.
+          LEFT: indica desde empieza desde el margen izquierdo del imageview, 0 empezará desde el incio
+                y si se aumenta ira acortandola en ancho
+        * */
+        Rect dest1 = new Rect(0, 0, 274, 290); // left,top,right,bottom
+        comboImage.drawBitmap(c, null, dest1, null);
+        Rect dest2 = new Rect(0, 0, 274, 290);
+        comboImage.drawBitmap(s, null, dest2, null);
+        return bmOverlay;
     }
 }
 
 
+//Bitmap bitmap = Bitmap.createBitmap(imagenAvatar.getWidth(),imagenAvatar.getHeight()/4,Bitmap.Config.RGB_565);
